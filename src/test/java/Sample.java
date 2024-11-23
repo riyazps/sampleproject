81981784b6aa3b2229e95d3d385cb3bb8985c80e
riyazps <riyaz777aero@gmail.com>
2024-11-23 20:47:54 +0530

    hello

---
+++ src/test/java/Sample.java
@@ -0,0 +1,21 @@
+
+public class Sample 
+{
+	private void sample1() {
+		System.out.println("sample1");
+
+	}
+	private void sample2() {
+		System.out.println("sample2");
+	}
+	private void sample3() {
+		System.out.println("sample3");
+		}
+	public static void main(String[] args) {
+		Sample s= new Sample();
+		s.sample1();
+		s.sample2();
+		s.sample3();
+	}
+
+}
